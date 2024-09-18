e6dbb39b23aad93663131db5a77ce1dee18dfbaa
BrandonHo419 <brandon.ho0419@gmail.com>
2024-09-18 20:49:49 +0930

    Basic window done.
    This is the base window.
    The playable window.
    Right there is an avocado. + beetroot.. credits go to my friend.
    We can create different sprites for buttons.
    Right now the click logic is done (more or less)
    
    It doesn't call any proper functs.

---
+++ IntroWindow.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <ctime>
+
+using namespace std;
+using namespace sf;
+
+void clickTest() {
+    cout << "Click Testing Successful" << endl;
+}
+
+int main() {
+    
+
+    RenderWindow win(VideoMode(1280, 720), "Super Awesome Plant Game :3");
+
+    // Load textures
+    Texture bd; // create something for backdrop
+    Texture minibd;
+    Texture avo;
+    Texture beet;
+    // End loading textures
+
+    // load from file
+    bd.loadFromFile("bd1.jpg");
+    minibd.loadFromFile("minibd.jpg");
+    avo.loadFromFile("avocado.png");
+    beet.loadFromFile("beetroot.png");
+    // end load from file
+    
+
+
+    if (!bd.loadFromFile("bd1.jpg")) {
+        cerr << "Error loading background texture" << endl;
+        return -1; // Exit if the texture fails to load
+    }
+
+    if (!minibd.loadFromFile("minibd.jpg")) {
+        cerr << "Error loading background texture" << endl;
+        return -1; 
+    }
+    
+
+    // Create sprites for the GFX
+    Sprite backdrop(bd); // creates a sprite so it stays in systme memory
+    Sprite minibackdrop(minibd);
+    Sprite buttonTest(minibd);
+    Sprite avocado(avo);
+    Sprite beetroot(beet);
+    
+
+    // End sprites
+
+    // Set Scales
+    minibackdrop.setScale(2.0, 1.5);
+    buttonTest.setScale(0.25, 0.15);
+    avocado.setScale(0.2, 0.2);
+    beetroot.setScale(0.2, 0.2);
+    //End scales
+
+    // Set positions
+    FloatRect minibackdropbounds = minibackdrop.getLocalBounds(); // Get the local boudns with FloatRect which gets dimension (width and height)
+    minibackdrop.setOrigin(minibackdropbounds.width/2, minibackdropbounds.height/2); // this ensures it calculates relative to the center
+    minibackdrop.setPosition(win.getSize().x/2, win.getSize().y/2); // sets the position
+
+    // New position
+    buttonTest.setPosition(100, 100);
+    avocado.setPosition(150, 100);
+    beetroot.setPosition(250, 200);
+
+    // End positions
+
+
+    while (win.isOpen())
+    {
+        sf::Event event;
+        while (win.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                win.close();
+        if(event.type == Event::MouseButtonPressed) { // checks if it is a mouse press
+            if(event.mouseButton.button == Mouse::Left) { // checks whether it was a left mouse button press
+                Vector2i mousepos = Mouse::getPosition(win); // retrieves the current mouse position (x and y)
+                if(buttonTest.getGlobalBounds().contains(static_cast<Vector2f>(mousepos))) { // returns a rectangle of bounds and static_cast converts 2i to 2f
+                    clickTest(); // calls function
+                }
+            }
+        }
+    };
+
+        // Create base rectangle shapes
+
+
+        win.clear();
+        win.draw(backdrop);
+        win.draw(minibackdrop);
+        win.draw(buttonTest);
+        win.draw(avocado);
+        win.draw(beetroot);
+        win.display();
+    }
+
+    return 0;
+}
+
