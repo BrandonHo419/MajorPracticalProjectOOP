1de2d6337ce23a644d3dc798c04498ee95c06a75
BrandonHo419 <brandon.ho0419@gmail.com>
2024-09-26 13:33:16 +0930

    Update IntroWindow.cpp

--- IntroWindow.cpp
+++ IntroWindow.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <ctime>
-#include "Textures.h"
-#include "drawShop.h"
-#include "PlantGrowth.h"
-#include "Player.h"
-#include "Shop.h"
+#include "Requirements.h"
 
 using namespace std;
 using namespace sf;
@@ -194,18 +187,8 @@ int main() {
 
 
     // load from file
-    setupTextures(bd, minibd);
-    avo.loadFromFile("avocado.png");
-    beet.loadFromFile("beetroot.png");
-    apple.loadFromFile("apple.png");
-    banana.loadFromFile("banana.png");
-    cherry.loadFromFile("cherry.png");
-    shopButton.loadFromFile("shop.png");
-    sign.loadFromFile("sign.png");
-    sign2.loadFromFile("sign.png");
-    sign3.loadFromFile("sign.png");
-    sign4.loadFromFile("sign.png");
-    sign5.loadFromFile("sign.png");
+    setupTextures(bd, minibd, avo, beet, apple, banana, cherry, shopButton, sign, sign2, sign3, sign4, sign5);
+    
     // end load from file
     
 
@@ -246,6 +229,7 @@ int main() {
     Sprite signs4(sign4);
     Sprite signs5(sign5);
 
+// Shop Sprites
     Sprite background(back);
     Sprite foreground(fore);
     Sprite shopSign1(board1);
@@ -259,19 +243,9 @@ int main() {
     // End sprites
 
     // Set Scales
-    minibackdrop.setScale(2.0, 1.5);
-    avocado.setScale(0.14, 0.14);
-    beetroot.setScale(0.14, 0.14);
-    apples.setScale(0.14, 0.14);
-    bananas.setScale(0.14, 0.14);
-    cherries.setScale(0.2, 0.2);
-    shop.setScale(0.4, 0.4);
-    signs.setScale(0.4, 0.4);
-    signs2.setScale(0.4, 0.4);
-    signs3.setScale(0.4, 0.4);
-    signs4.setScale(0.4, 0.4);
-    signs5.setScale(0.4, 0.4);
-
+    setScales(win, minibackdrop, avocado, beetroot, apples, bananas, cherries, shop, signs, signs2, signs3, signs4, signs5); // also sets positions of these
+    
+    // shop scales
     foreground.setScale(2.0, 1.5);
     fert.setScale(0.2f, 0.2f);
     greenhouse.setScale(0.35f, 0.35f);
@@ -283,34 +257,7 @@ int main() {
     //End scales
 
     // Set positions
-    FloatRect minibackdropbounds = minibackdrop.getLocalBounds(); // Get the local boudns with FloatRect which gets dimension (width and height)
-    minibackdrop.setOrigin(minibackdropbounds.width/2, minibackdropbounds.height/2); // this ensures it calculates relative to the center
-    minibackdrop.setPosition(win.getSize().x/2, win.getSize().y/2); // sets the position
-
-    FloatRect shopPlace = shop.getLocalBounds();
-    shop.setOrigin(shopPlace.width/2, shopPlace.height/2);
-    shop.setPosition(win.getSize().x/2, win.getSize().y/2 +235);
-
-    FloatRect signsPlacement = signs.getLocalBounds();
-    signs.setOrigin(signsPlacement.width/2, signsPlacement.height/2);
-    signs.setPosition(win.getSize().x/2 - 458, signsPlacement.height/2 - 50);
     
-
-    FloatRect signsPlacement2 = signs2.getLocalBounds();
-    signs2.setOrigin(signsPlacement2.width/2, signsPlacement2.height/2);
-    signs2.setPosition(win.getSize().x/2 - 458, signsPlacement2.height/2 + 50);
-
-    FloatRect signsPlacement3 = signs3.getLocalBounds();
-    signs3.setOrigin(signsPlacement3.width/2, signsPlacement3.height/2);
-    signs3.setPosition(win.getSize().x/2 - 458, signsPlacement3.height/2 + 150);
-
-    FloatRect signsPlacement4 = signs4.getLocalBounds();
-    signs4.setOrigin(signsPlacement4.width/2, signsPlacement4.height/2);
-    signs4.setPosition(win.getSize().x/2 - 453, signsPlacement4.height/2 + 250);
-
-    FloatRect signsPlacement5 = signs5.getLocalBounds();
-    signs5.setOrigin(signsPlacement5.width/2, signsPlacement5.height/2);
-    signs5.setPosition(win.getSize().x/2 - 458, signsPlacement5.height/2 + 357);
 
     FloatRect minibackdropbounds2 = foreground.getLocalBounds();
     foreground.setOrigin(minibackdropbounds2.width / 2, minibackdropbounds2.height / 2);
